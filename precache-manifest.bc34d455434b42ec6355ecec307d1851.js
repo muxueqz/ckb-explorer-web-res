34fe04e8d7b61927e4f33d3d6b14128b3d7a1ad1
root <root@ip-172-31-7-167.ap-east-1.compute.internal>
2020-03-10 01:53:55 +0800

    first commit

---
+++ precache-manifest.bc34d455434b42ec6355ecec307d1851.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28df5fef66638d331084bbcbd188cb7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904b198eccc8cc78bb70",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "f67bcfe73a7ffefff4ed",
+    "url": "/static/css/main.5e506eed.chunk.css"
+  },
+  {
+    "revision": "904b198eccc8cc78bb70",
+    "url": "/static/js/2.c0931c82.chunk.js"
+  },
+  {
+    "revision": "8d5f7589a9027e05629570c8dd3da9dd",
+    "url": "/static/js/2.c0931c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67bcfe73a7ffefff4ed",
+    "url": "/static/js/main.af93643f.chunk.js"
+  },
+  {
+    "revision": "c0d7b120cfec511de8d8",
+    "url": "/static/js/runtime-main.01aa1383.js"
+  },
+  {
+    "revision": "f032707031fdd56b9818ad15ae300c98",
+    "url": "/static/media/blue_mobile_404.f0327070.png"
+  },
+  {
+    "revision": "b8021239d53ea8a199d3995f4dedacd1",
+    "url": "/static/media/blue_mobile_maintain.b8021239.png"
+  },
+  {
+    "revision": "4b04a4d13c42ec2309a585031d8fe56f",
+    "url": "/static/media/blue_pc_404.4b04a4d1.png"
+  },
+  {
+    "revision": "264db53aecd43221c3a1c7bed6be1d9e",
+    "url": "/static/media/blue_pc_maintain.264db53a.png"
+  },
+  {
+    "revision": "42549850e089887a15fe631222649d42",
+    "url": "/static/media/mobile_data_not_found.42549850.png"
+  },
+  {
+    "revision": "b7742f32901c02fb3ce2e5bf1c879d3f",
+    "url": "/static/media/mobile_maintain.b7742f32.png"
+  },
+  {
+    "revision": "121bf817dd5ef207fec0a5ba11d8c23c",
+    "url": "/static/media/pc_404.121bf817.png"
+  },
+  {
+    "revision": "851ef0e7b43c6f7b06df11b7552f144f",
+    "url": "/static/media/pc_data_not_found.851ef0e7.png"
+  },
+  {
+    "revision": "34a799169d42fc414c35fa7e20663661",
+    "url": "/static/media/pc_maintain.34a79916.png"
+  }
+]);
