aca80a920f7bbb2667beaec05b50476736aa07a7
Ubuntu <ubuntu@ip-172-31-7-167.ap-east-1.compute.internal>
2020-03-15 23:08:50 +0800

    first commit

---
+++ precache-manifest.d29ab750d841de8a04a7cedd7124428d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d4a261f1a8e43bccef6e5dbd691bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f02813cd448570a10562",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "60448b5a6733288a4263",
+    "url": "/static/css/main.5e506eed.chunk.css"
+  },
+  {
+    "revision": "f02813cd448570a10562",
+    "url": "/static/js/2.db6c932d.chunk.js"
+  },
+  {
+    "revision": "8d5f7589a9027e05629570c8dd3da9dd",
+    "url": "/static/js/2.db6c932d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60448b5a6733288a4263",
+    "url": "/static/js/main.43987545.chunk.js"
+  },
+  {
+    "revision": "c0d7b120cfec511de8d8",
+    "url": "/static/js/runtime-main.01aa1383.js"
+  },
+  {
+    "revision": "f032707031fdd56b9818ad15ae300c98",
+    "url": "/static/media/blue_mobile_404.f0327070.png"
+  },
+  {
+    "revision": "b8021239d53ea8a199d3995f4dedacd1",
+    "url": "/static/media/blue_mobile_maintain.b8021239.png"
+  },
+  {
+    "revision": "4b04a4d13c42ec2309a585031d8fe56f",
+    "url": "/static/media/blue_pc_404.4b04a4d1.png"
+  },
+  {
+    "revision": "264db53aecd43221c3a1c7bed6be1d9e",
+    "url": "/static/media/blue_pc_maintain.264db53a.png"
+  },
+  {
+    "revision": "42549850e089887a15fe631222649d42",
+    "url": "/static/media/mobile_data_not_found.42549850.png"
+  },
+  {
+    "revision": "b7742f32901c02fb3ce2e5bf1c879d3f",
+    "url": "/static/media/mobile_maintain.b7742f32.png"
+  },
+  {
+    "revision": "121bf817dd5ef207fec0a5ba11d8c23c",
+    "url": "/static/media/pc_404.121bf817.png"
+  },
+  {
+    "revision": "851ef0e7b43c6f7b06df11b7552f144f",
+    "url": "/static/media/pc_data_not_found.851ef0e7.png"
+  },
+  {
+    "revision": "34a799169d42fc414c35fa7e20663661",
+    "url": "/static/media/pc_maintain.34a79916.png"
+  }
+]);
