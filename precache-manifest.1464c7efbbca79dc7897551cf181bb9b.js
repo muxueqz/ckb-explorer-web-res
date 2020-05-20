89f197a76413e18bbee6b0a583501cf46378f829
CI <no@no.no>
2020-05-20 14:35:13 +0800

    first commit

---
+++ precache-manifest.1464c7efbbca79dc7897551cf181bb9b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9d5fb2f80b5e9583b8412fb9ba0213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f19361e8950532f88716",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "7aecfc889f545e529e3a",
+    "url": "/static/css/main.a68cc5a5.chunk.css"
+  },
+  {
+    "revision": "f19361e8950532f88716",
+    "url": "/static/js/2.57e9a81e.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.57e9a81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aecfc889f545e529e3a",
+    "url": "/static/js/main.170c8162.chunk.js"
+  },
+  {
+    "revision": "6a6adb408bcb20b6f8ce",
+    "url": "/static/js/runtime-main.573d8b58.js"
+  },
+  {
+    "revision": "f032707031fdd56b9818ad15ae300c98",
+    "url": "/static/media/blue_mobile_404.f0327070.png"
+  },
+  {
+    "revision": "b8021239d53ea8a199d3995f4dedacd1",
+    "url": "/static/media/blue_mobile_maintain.b8021239.png"
+  },
+  {
+    "revision": "4b04a4d13c42ec2309a585031d8fe56f",
+    "url": "/static/media/blue_pc_404.4b04a4d1.png"
+  },
+  {
+    "revision": "264db53aecd43221c3a1c7bed6be1d9e",
+    "url": "/static/media/blue_pc_maintain.264db53a.png"
+  },
+  {
+    "revision": "de14dec04d76d3450e8647f94f68608f",
+    "url": "/static/media/home_background.de14dec0.svg"
+  },
+  {
+    "revision": "42549850e089887a15fe631222649d42",
+    "url": "/static/media/mobile_data_not_found.42549850.png"
+  },
+  {
+    "revision": "b7742f32901c02fb3ce2e5bf1c879d3f",
+    "url": "/static/media/mobile_maintain.b7742f32.png"
+  },
+  {
+    "revision": "121bf817dd5ef207fec0a5ba11d8c23c",
+    "url": "/static/media/pc_404.121bf817.png"
+  },
+  {
+    "revision": "851ef0e7b43c6f7b06df11b7552f144f",
+    "url": "/static/media/pc_data_not_found.851ef0e7.png"
+  },
+  {
+    "revision": "34a799169d42fc414c35fa7e20663661",
+    "url": "/static/media/pc_maintain.34a79916.png"
+  }
+]);
